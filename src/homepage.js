8f7127ad7337f28c75287dae87b3db9f13336f9c
HangorG <gaba93@yandex.ru>
2021-02-06 00:32:09 +0300

    homepage module

---
+++ src/homepage.js
@@ -0,0 +1,39 @@
+function renderHomepage() {
+  // create nav el
+  const mainContainer = document.querySelector("#content");
+  const nav = defineElement("nav");
+  const links = defineElement("div", "links");
+  const about = defineElement("p", "about", "About");
+  const menu = defineElement("p", "menu", "Menu");
+  const contact = defineElement("p", "contact", "Contact");
+
+  links.append(about, menu, contact);
+  nav.appendChild(links);
+
+  // create main el
+  const infoText =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet distinctio sint similique soluta reiciendis adipisci! Vero et, dolor eum a quam autem nostrum, accusamus, incidunt sint ratione fuga possimus laborum.";
+  const infoText1 =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptate voluptatem inventore, totam laboriosam recusandae, ipsam, harum nulla repellat fugiat commodi culpa. Impedit laborum est animi modi eos voluptatibus eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quidem a expedita veritatis. Amet cupiditate tenetur numquam laudantium iure dignissimos vel repudiandae fugiat doloribus omnis, ullam aspernatur assumenda accusamus impedit?";
+
+  const main = defineElement("main");
+  const info = defineElement("div", "info", infoText);
+  const info1 = defineElement("div", "info1", infoText1);
+  const picture = defineElement("div", "picture");
+  const img = defineElement("img", null, null, "burger.svg");
+
+  picture.appendChild(img);
+  main.append(info, info1, picture);
+  mainContainer.append(nav, main);
+}
+
+function defineElement(definition, className, innerContent, source) {
+  const element = document.createElement(definition);
+  element.className = className;
+  element.textContent = innerContent;
+  element.src = source;
+
+  return element;
+}
+
+export default renderHomepage;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import renderHomepage from './homepage'
+
+renderHomepage();
